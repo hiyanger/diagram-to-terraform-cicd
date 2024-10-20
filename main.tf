9bb48a9b96bab58a50096263d90bbfacfdc79c7d
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-10-20 21:18:06 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
+locals {
+  aws_access_key_id     = "YOUR_ACCESS_KEY_ID"
+  aws_secret_access_key = "YOUR_SECRET_ACCESS_KEY"
+}
+
 provider "aws" {
-  region = "ap-northeast-1"
+  region     = "ap-northeast-1"
+  access_key = local.aws_access_key_id
+  secret_key = local.aws_secret_access_key
 }
 
 resource "aws_vpc" "diagram" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
   tags = {
     Name = "diagram-vpc"
   }
@@ -15,7 +19,6 @@ resource "aws_vpc" "diagram" {
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.diagram.id
   cidr_block = "10.0.1.0/24"
-
   tags = {
     Name = "diagram-public-subnet"
   }
@@ -23,7 +26,6 @@ resource "aws_subnet" "public" {
 
 resource "aws_internet_gateway" "diagram" {
   vpc_id = aws_vpc.diagram.id
-
   tags = {
     Name = "diagram-igw"
   }
@@ -31,12 +33,10 @@ resource "aws_internet_gateway" "diagram" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.diagram.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.diagram.id
   }
-
   tags = {
     Name = "diagram-public-rt"
   }
