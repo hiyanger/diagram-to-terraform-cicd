6bffe78e8bf6379253eb2ce935e778ce0b101f12
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-10-21 00:53:40 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -14,9 +14,7 @@ provider "aws" {
 
 resource "aws_vpc" "diagram" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
+  
   tags = {
     Name = "diagram-vpc"
   }
