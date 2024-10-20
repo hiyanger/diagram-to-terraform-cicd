76d0a577ea56f254f1c5904cefed863d05b6d504
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-10-20 16:49:05 +0900

    Add generated Terraform code from Bedrock

--- generated_terraform_code.tf
+++ generated_terraform_code.tf
@@ -4,19 +4,15 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-
+  
   tags = {
-    Name = "main-vpc"
+    Name = "Main VPC"
   }
 }
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-northeast-1a"
-  map_public_ip_on_launch = true
 
   tags = {
     Name = "Public Subnet"
