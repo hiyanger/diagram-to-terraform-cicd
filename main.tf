a0d543348268c79cee4f70b22d5c2b73dec2a248
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-10-23 21:54:16 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -19,18 +19,18 @@ resource "aws_vpc" "diagram" {
   }
 }
 
+resource "aws_internet_gateway" "diagram" {
+  vpc_id = aws_vpc.diagram.id
+  tags = {
+    Name = "diagram-igw"
+  }
+}
+
 resource "aws_subnet" "diagram" {
   vpc_id     = aws_vpc.diagram.id
   cidr_block = "10.0.1.0/24"
   tags = {
     Name = "diagram-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "diagram" {
-  vpc_id = aws_vpc.diagram.id
-  tags = {
-    Name = "diagram-igw"
   }
 }
 
