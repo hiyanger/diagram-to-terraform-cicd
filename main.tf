535003c9bedfd497fa17b3404601085fd2efdfa2
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-23 20:03:11 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -7,35 +7,15 @@ variable "aws_secret_access_key" {
 }
 
 provider "aws" {
-  region = "ap-northeast-1"
+  region     = "ap-northeast-1"
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_security_group" "deploy" {
-  name = "deploy-sg"
-  description = "deploy security group"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # 適宜変更
-  }
+resource "aws_s3_bucket" "deploy" {
+  bucket = "20241123_awsevents3"
 
   tags = {
-    Name = "deploy-sg"
-  }
-}
-
-resource "aws_instance" "deploy" {
-  ami                         = "ami-03f584e50b2d32776"  # AL2023
-  instance_type              = "t2.micro"
-  key_name                   = "hiyama-diagram"
-  vpc_security_group_ids     = [aws_security_group.deploy.id]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "deploy-ec2"
+    Name = "deploy-s3"
   }
 }
