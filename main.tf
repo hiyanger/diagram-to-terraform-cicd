36f485110e1f37f88d3d5a8ea2bc36b0d5d4b3e5
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-23 20:00:54 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -14,24 +14,26 @@ provider "aws" {
 
 resource "aws_s3_bucket" "deploy" {
   bucket = "deploy-bucket"
-
-  tags = {
-    Name = "deploy-s3"
-  }
 }
 
 resource "aws_cloudfront_distribution" "deploy" {
+  enabled = true
+  
   origin {
     domain_name = aws_s3_bucket.deploy.bucket_regional_domain_name
     origin_id   = "S3Origin"
+    
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.deploy.cloudfront_access_identity_path
+    }
   }
-
-  enabled = true
+  
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = "S3Origin"
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
+    
     forwarded_values {
       query_string = false
       cookies {
@@ -39,19 +41,33 @@ resource "aws_cloudfront_distribution" "deploy" {
       }
     }
   }
-
+  
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
   }
-
+  
   viewer_certificate {
     cloudfront_default_certificate = true
   }
-
+  
   tags = {
     Name = "deploy-cloudfront"
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "deploy" {}
+
+resource "aws_lambda_function" "deploy" {
+  filename         = "lambda.zip"
+  function_name    = "deploy-lambda"
+  role             = aws_iam_role.lambda.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  
+  tags = {
+    Name = "deploy-lambda"
   }
 }
 
@@ -59,20 +75,20 @@ resource "aws_dynamodb_table" "deploy" {
   name           = "deploy-table"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
-
+  
   attribute {
     name = "id"
     type = "S"
   }
-
+  
   tags = {
     Name = "deploy-dynamodb"
   }
 }
 
 resource "aws_iam_role" "lambda" {
-  name = "lambda_role"
-
+  name = "deploy-lambda-role"
+  
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -85,63 +101,38 @@ resource "aws_iam_role" "lambda" {
       }
     ]
   })
-
+  
   tags = {
     Name = "deploy-lambda-role"
   }
 }
 
-resource "aws_lambda_function" "deploy" {
-  filename         = "lambda.zip"
-  function_name    = "deploy-function"
-  role            = aws_iam_role.lambda.arn
-  handler         = "index.handler"
-  runtime         = "nodejs18.x"
-
-  tags = {
-    Name = "deploy-lambda"
-  }
-}
-
-resource "aws_vpc" "deploy" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_instance" "deploy" {
+  ami           = "ami-03f584e50b2d32776" # AL2023
+  instance_type = "t2.micro"
+  key_name      = "hiyama-diagram"
+  
+  vpc_security_group_ids = [aws_security_group.deploy.id]
+  
+  associate_public_ip_address = true
   
   tags = {
-    Name = "deploy-vpc"
-  }
-}
-
-resource "aws_subnet" "deploy" {
-  vpc_id     = aws_vpc.deploy.id
-  cidr_block = "10.0.1.0/24"
-  
-  tags = {
-    Name = "deploy-subnet"
+    Name = "deploy-ec2"
   }
 }
 
 resource "aws_security_group" "deploy" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.deploy.id
-
+  name        = "deploy-sg"
+  description = "Security group for EC2"
+  
   ingress {
-    description = "SSH from anywhere"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # 適宜変更
+    cidr_blocks = ["0.0.0.0/0"] # 適宜変更
   }
-
+  
   tags = {
     Name = "deploy-sg"
   }
 }
-
-resource "aws_instance" "deploy" {
-  ami           = "ami-03f584e50b2d32776"  # AL2023
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.deploy.id
-  key_name      = "hiyama-diagram"
-  
-  vpc_security_group_ids = [aws_security_group.deploy
