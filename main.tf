afcdc2804de82fa998de8e49adb1c00559ead09f
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-23 19:49:12 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -12,36 +12,10 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_s3_bucket" "diagram" {
-  bucket = "hiyama-bedrock-20241123-1"
+resource "aws_s3_bucket" "deploy" {
+  bucket = "hiyama-bedrock-20241123-5"
 
   tags = {
-    Name = "diagram-s3"
+    Name = "deploy-s3"
   }
-}
-
-resource "aws_s3_bucket_versioning" "diagram" {
-  bucket = aws_s3_bucket.diagram.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "diagram" {
-  bucket = aws_s3_bucket.diagram.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "diagram" {
-  bucket = aws_s3_bucket.diagram.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
