5ed81397632e13e6283eb20f89574ec56a301ebf
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-24 19:55:35 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable "aws_secret_access_key" {
 }
 
 provider "aws" {
-  region     = "ap-northeast-1"
+  region = "ap-northeast-1"
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
