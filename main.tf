dcc23d7ee82ef592f4d437dfc27c46ba4fb858fb
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-26 05:10:12 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -7,79 +7,65 @@ variable "aws_secret_access_key" {
 }
 
 provider "aws" {
-  region     = "ap-northeast-1"
+  region = "ap-northeast-1"
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_ecs_cluster" "deploy" {
-  name = "ecs-deploy"
-  tags = {
-    Name = "deploy-ecs"
-  }
-}
-
-resource "aws_ecs_task_definition" "deploy" {
-  family                   = "deploy"
-  requires_compatibilities = ["FARGATE"]
-  network_mode            = "awsvpc"
-  cpu                     = 256
-  memory                  = 512
-
-  container_definitions = jsonencode([{
-    name  = "deploy"
-    image = "nginx:latest"
-  }])
-}
-
 resource "aws_vpc" "deploy" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
   tags = {
     Name = "deploy-vpc"
   }
 }
 
-resource "aws_subnet" "deploy" {
-  vpc_id            = aws_vpc.deploy.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-northeast-1a"
-  tags = {
-    Name = "deploy-subnet"
-  }
-}
-
 resource "aws_internet_gateway" "deploy" {
   vpc_id = aws_vpc.deploy.id
+
   tags = {
     Name = "deploy-igw"
   }
 }
 
+resource "aws_subnet" "deploy" {
+  vpc_id = aws_vpc.deploy.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "deploy-subnet"
+  }
+}
+
 resource "aws_route_table" "deploy" {
   vpc_id = aws_vpc.deploy.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.deploy.id
   }
+
   tags = {
-    Name = "deploy-rt"
+    Name = "deploy-rtb"
   }
 }
 
 resource "aws_route_table_association" "deploy" {
-  subnet_id      = aws_subnet.deploy.id
+  subnet_id = aws_subnet.deploy.id
   route_table_id = aws_route_table.deploy.id
 }
 
 resource "aws_security_group" "deploy" {
-  name   = "deploy-sg"
+  name = "deploy-sg"
   vpc_id = aws_vpc.deploy.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # 適宜変更
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # 適宜変更
   }
 
   tags = {
@@ -88,14 +74,21 @@ resource "aws_security_group" "deploy" {
 }
 
 resource "aws_instance" "deploy" {
-  ami                         = "ami-03f584e50b2d32776" # AL2023
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.deploy.id
-  vpc_security_group_ids      = [aws_security_group.deploy.id]
-  associate_public_ip_address = true
-  key_name                    = "hiyama-diagram"
+  ami = "ami-03f584e50b2d32776"  # AL2023
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.deploy.id
+  key_name = "hiyama-diagram"
+  vpc_security_group_ids = [aws_security_group.deploy.id]
 
   tags = {
     Name = "deploy-ec2"
+  }
+}
+
+resource "aws_s3_bucket" "deploy" {
+  bucket = "deploy-bucket"
+
+  tags = {
+    Name = "deploy-s3"
   }
 }
