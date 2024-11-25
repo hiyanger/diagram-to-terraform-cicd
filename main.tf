8d54851f59d41d336d3f77065fcdf87e44aa3a6a
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-26 05:50:28 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -7,81 +7,111 @@ variable "aws_secret_access_key" {
 }
 
 provider "aws" {
-  region     = "ap-northeast-1"
+  region = "ap-northeast-1"
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_instance" "deploy" {
-  ami           = "ami-03f584e50b2d32776"  # AL2023
-  instance_type = "t2.micro"
-  key_name      = "hiyama-diagram"
+resource "aws_dynamodb_table" "main" {
+  name           = "deploy-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
 
-  subnet_id                   = aws_subnet.deploy.id
-  vpc_security_group_ids      = [aws_security_group.deploy.id]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "deploy-ec2"
-  }
-}
-
-resource "aws_vpc" "deploy" {
-  cidr_block = "10.0.0.0/16"
-  
-  tags = {
-    Name = "deploy-vpc"
-  }
-}
-
-resource "aws_subnet" "deploy" {
-  vpc_id     = aws_vpc.deploy.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "deploy-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "deploy" {
-  vpc_id = aws_vpc.deploy.id
-
-  tags = {
-    Name = "deploy-igw"
-  }
-}
-
-resource "aws_route_table" "deploy" {
-  vpc_id = aws_vpc.deploy.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.deploy.id
+  attribute {
+    name = "id"
+    type = "S"
   }
 
   tags = {
-    Name = "deploy-rt"
+    Name = "deploy-dynamodb"
   }
 }
 
-resource "aws_route_table_association" "deploy" {
-  subnet_id      = aws_subnet.deploy.id
-  route_table_id = aws_route_table.deploy.id
-}
-
-resource "aws_security_group" "deploy" {
-  name        = "deploy-sg"
-  description = "Security group for SSH access"
-  vpc_id      = aws_vpc.deploy.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # 適宜変更
-  }
+resource "aws_lambda_function" "main" {
+  filename         = "lambda.zip"
+  function_name    = "deploy-function"
+  role            = aws_iam_role.lambda.arn
+  handler         = "index.handler"
+  runtime         = "nodejs18.x"
 
   tags = {
-    Name = "deploy-sg"
+    Name = "deploy-lambda"
   }
+}
+
+resource "aws_iam_role" "lambda" {
+  name = "deploy-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "deploy-lambda-role"
+  }
+}
+
+resource "aws_api_gateway_rest_api" "main" {
+  name = "deploy-api"
+
+  tags = {
+    Name = "deploy-apigateway"
+  }
+}
+
+resource "aws_api_gateway_resource" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "main" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.main.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.main.id
+  http_method = aws_api_gateway_method.main.http_method
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.main.invoke_arn
+  integration_http_method = "POST"
+}
+
+resource "aws_api_gateway_deployment" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+
+  depends_on = [
+    aws_api_gateway_integration.main
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "main" {
+  deployment_id = aws_api_gateway_deployment.main.id
+  rest_api_id  = aws_api_gateway_rest_api.main.id
+  stage_name   = "prod"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.main.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.main.execution_arn}/*/*"
 }
