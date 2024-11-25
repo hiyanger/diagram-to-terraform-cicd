8a1596bfca8670fbbffdbeccfa0ecdceb669f850
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-26 05:47:49 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -12,13 +12,34 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_vpc" "deploy" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
+resource "aws_instance" "deploy" {
+  ami           = "ami-03f584e50b2d32776"  # AL2023
+  instance_type = "t2.micro"
+  key_name      = "hiyama-diagram"
+
+  subnet_id                   = aws_subnet.deploy.id
+  vpc_security_group_ids      = [aws_security_group.deploy.id]
+  associate_public_ip_address = true
 
   tags = {
+    Name = "deploy-ec2"
+  }
+}
+
+resource "aws_vpc" "deploy" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
     Name = "deploy-vpc"
+  }
+}
+
+resource "aws_subnet" "deploy" {
+  vpc_id     = aws_vpc.deploy.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "deploy-subnet"
   }
 }
 
@@ -27,16 +48,6 @@ resource "aws_internet_gateway" "deploy" {
 
   tags = {
     Name = "deploy-igw"
-  }
-}
-
-resource "aws_subnet" "deploy" {
-  vpc_id = aws_vpc.deploy.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-northeast-1a"
-
-  tags = {
-    Name = "deploy-subnet"
   }
 }
 
@@ -49,48 +60,28 @@ resource "aws_route_table" "deploy" {
   }
 
   tags = {
-    Name = "deploy-rtb"
+    Name = "deploy-rt"
   }
 }
 
 resource "aws_route_table_association" "deploy" {
-  subnet_id = aws_subnet.deploy.id
+  subnet_id      = aws_subnet.deploy.id
   route_table_id = aws_route_table.deploy.id
 }
 
 resource "aws_security_group" "deploy" {
-  name   = "deploy-sg"
-  vpc_id = aws_vpc.deploy.id
+  name        = "deploy-sg"
+  description = "Security group for SSH access"
+  vpc_id      = aws_vpc.deploy.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # 適宜変更
+    cidr_blocks = ["0.0.0.0/0"]  # 適宜変更
   }
 
   tags = {
     Name = "deploy-sg"
-  }
-}
-
-resource "aws_instance" "deploy" {
-  ami           = "ami-03f584e50b2d32776" # AL2023
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.deploy.id
-  vpc_security_group_ids = [aws_security_group.deploy.id]
-  key_name      = "hiyama-diagram"
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "deploy-ec2"
-  }
-}
-
-resource "aws_s3_bucket" "deploy" {
-  bucket = "deploy-bucket"
-
-  tags = {
-    Name = "deploy-s3"
   }
 }
