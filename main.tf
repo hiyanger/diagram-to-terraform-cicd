16aa769490d3caeb8a0a91f6f666e70fe87bfad9
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-26 04:36:21 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -14,9 +14,8 @@ provider "aws" {
 
 resource "aws_vpc" "deploy" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
   enable_dns_hostnames = true
-  enable_dns_support   = true
-
   tags = {
     Name = "deploy-vpc"
   }
@@ -24,17 +23,16 @@ resource "aws_vpc" "deploy" {
 
 resource "aws_internet_gateway" "deploy" {
   vpc_id = aws_vpc.deploy.id
-
   tags = {
     Name = "deploy-igw"
   }
 }
 
 resource "aws_subnet" "deploy" {
-  vpc_id            = aws_vpc.deploy.id
-  cidr_block        = "10.0.1.0/24"
+  vpc_id = aws_vpc.deploy.id
+  cidr_block = "10.0.1.0/24"
   availability_zone = "ap-northeast-1a"
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "deploy-subnet"
   }
@@ -42,30 +40,28 @@ resource "aws_subnet" "deploy" {
 
 resource "aws_route_table" "deploy" {
   vpc_id = aws_vpc.deploy.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.deploy.id
   }
-
   tags = {
-    Name = "deploy-rtb"
+    Name = "deploy-rt"
   }
 }
 
 resource "aws_route_table_association" "deploy" {
-  subnet_id      = aws_subnet.deploy.id
+  subnet_id = aws_subnet.deploy.id
   route_table_id = aws_route_table.deploy.id
 }
 
 resource "aws_security_group" "deploy" {
-  name   = "deploy-sg"
+  name = "deploy-sg"
   vpc_id = aws_vpc.deploy.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # 適宜変更
   }
 
@@ -75,23 +71,13 @@ resource "aws_security_group" "deploy" {
 }
 
 resource "aws_instance" "deploy" {
-  ami           = "ami-03f584e50b2d32776" # AL2023
+  ami = "ami-03f584e50b2d32776" # AL2023
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.deploy.id
-  key_name      = "hiyama-diagram"
-
-  associate_public_ip_address = true
-  vpc_security_group_ids     = [aws_security_group.deploy.id]
+  subnet_id = aws_subnet.deploy.id
+  vpc_security_group_ids = [aws_security_group.deploy.id]
+  key_name = "hiyama-diagram"
 
   tags = {
     Name = "deploy-ec2"
-  }
-}
-
-resource "aws_s3_bucket" "deploy" {
-  bucket = "deploy-bucket"
-
-  tags = {
-    Name = "deploy-s3"
   }
 }
