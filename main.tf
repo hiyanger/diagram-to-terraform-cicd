2ad600408c1849e01d90e0d8ee0037e22c0c5d77
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-26 05:09:35 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -12,46 +12,37 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_iam_role" "deploy" {
-  name = "deploy"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
+resource "aws_ecs_cluster" "deploy" {
+  name = "ecs-deploy"
   tags = {
-    Name = "deploy-iam-role"
+    Name = "deploy-ecs"
   }
 }
 
-resource "aws_iam_role_policy_attachment" "deploy" {
-  role       = aws_iam_role.deploy.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_ecs_task_definition" "deploy" {
+  family                   = "deploy"
+  requires_compatibilities = ["FARGATE"]
+  network_mode            = "awsvpc"
+  cpu                     = 256
+  memory                  = 512
+
+  container_definitions = jsonencode([{
+    name  = "deploy"
+    image = "nginx:latest"
+  }])
 }
 
 resource "aws_vpc" "deploy" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
   tags = {
     Name = "deploy-vpc"
   }
 }
 
 resource "aws_subnet" "deploy" {
-  vpc_id     = aws_vpc.deploy.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.deploy.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-1a"
-
   tags = {
     Name = "deploy-subnet"
   }
@@ -59,7 +50,6 @@ resource "aws_subnet" "deploy" {
 
 resource "aws_internet_gateway" "deploy" {
   vpc_id = aws_vpc.deploy.id
-
   tags = {
     Name = "deploy-igw"
   }
@@ -67,14 +57,12 @@ resource "aws_internet_gateway" "deploy" {
 
 resource "aws_route_table" "deploy" {
   vpc_id = aws_vpc.deploy.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.deploy.id
   }
-
   tags = {
-    Name = "deploy-rtb"
+    Name = "deploy-rt"
   }
 }
 
@@ -84,15 +72,14 @@ resource "aws_route_table_association" "deploy" {
 }
 
 resource "aws_security_group" "deploy" {
-  name        = "deploy"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.deploy.id
+  name   = "deploy-sg"
+  vpc_id = aws_vpc.deploy.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # 適宜変更
+    cidr_blocks = ["0.0.0.0/0"] # 適宜変更
   }
 
   tags = {
@@ -101,35 +88,14 @@ resource "aws_security_group" "deploy" {
 }
 
 resource "aws_instance" "deploy" {
-  # AL2023
-  ami           = "ami-03f584e50b2d32776"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.deploy.id
-  key_name      = "hiyama-diagram"
+  ami                         = "ami-03f584e50b2d32776" # AL2023
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.deploy.id
+  vpc_security_group_ids      = [aws_security_group.deploy.id]
   associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.deploy.id]
+  key_name                    = "hiyama-diagram"
 
   tags = {
     Name = "deploy-ec2"
-  }
-}
-
-resource "aws_lambda_function" "deploy" {
-  filename      = "lambda_function.zip"
-  function_name = "deploy"
-  role          = aws_iam_role.deploy.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-
-  tags = {
-    Name = "deploy-lambda"
-  }
-}
-
-resource "aws_s3_bucket" "deploy" {
-  bucket = "deploy-bucket"
-
-  tags = {
-    Name = "deploy-s3"
   }
 }
