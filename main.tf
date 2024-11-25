35c221b45d09fbe17f65e2635642f376e4b909f2
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-26 04:32:18 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -12,66 +12,92 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_vpc" "deploy" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  tags = {
-    Name = "deploy-vpc"
-  }
-}
-
-resource "aws_subnet" "deploy" {
-  vpc_id            = aws_vpc.deploy.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-northeast-1a"
-  tags = {
-    Name = "deploy-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "deploy" {
-  vpc_id = aws_vpc.deploy.id
-  tags = {
-    Name = "deploy-igw"
-  }
-}
-
-resource "aws_route_table" "deploy" {
-  vpc_id = aws_vpc.deploy.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.deploy.id
-  }
-  tags = {
-    Name = "deploy-rtb"
-  }
-}
-
-resource "aws_route_table_association" "deploy" {
-  subnet_id      = aws_subnet.deploy.id
-  route_table_id = aws_route_table.deploy.id
-}
-
-resource "aws_security_group" "deploy" {
-  name   = "deploy-sg"
-  vpc_id = aws_vpc.deploy.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # 適宜変更
-  }
+resource "aws_lambda_function" "deploy" {
+  filename      = "lambda_function.zip"
+  function_name = "deploy"
+  role          = aws_iam_role.lambda.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
 
   tags = {
-    Name = "deploy-sg"
+    Name = "deploy-lambda"
   }
 }
 
+resource "aws_iam_role" "lambda" {
+  name = "lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = {
+    Name = "deploy-lambda-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_s3_bucket" "deploy" {
+  bucket = "deploy-bucket"
+
+  tags = {
+    Name = "deploy-s3"
+  }
+}
+
+resource "aws_api_gateway_rest_api" "deploy" {
+  name = "deploy-api"
+
+  tags = {
+    Name = "deploy-apigateway"
+  }
+}
+
+resource "aws_api_gateway_resource" "deploy" {
+  rest_api_id = aws_api_gateway_rest_api.deploy.id
+  parent_id   = aws_api_gateway_rest_api.deploy.root_resource_id
+  path_part   = "resource"
+}
+
+resource "aws_api_gateway_method" "deploy" {
+  rest_api_id   = aws_api_gateway_rest_api.deploy.id
+  resource_id   = aws_api_gateway_resource.deploy.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "deploy" {
+  rest_api_id = aws_api_gateway_rest_api.deploy.id
+  resource_id = aws_api_gateway_resource.deploy.id
+  http_method = aws_api_gateway_method.deploy.http_method
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.deploy.invoke_arn
+  integration_http_method = "POST"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.deploy.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.deploy.execution_arn}/*/*"
+}
+
+# AL2023
 resource "aws_instance" "deploy" {
-  ami           = "ami-03f584e50b2d32776"  # AL2023
+  ami           = "ami-03f584e50b2d32776"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.deploy.id
   key_name      = "hiyama-diagram"
   vpc_security_group_ids = [aws_security_group.deploy.id]
   associate_public_ip_address = true
@@ -80,3 +106,12 @@ resource "aws_instance" "deploy" {
     Name = "deploy-ec2"
   }
 }
+
+resource "aws_security_group" "deploy" {
+  name = "deploy-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks =
