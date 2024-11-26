096da13ff36dd844708a4b205c3c6f759a13d37c
hiyanger <34181755+hiyanger@users.noreply.github.com>
2024-11-26 12:36:24 +0900

    Add generated Terraform code from Bedrock

--- main.tf
+++ main.tf
@@ -13,8 +13,10 @@ provider "aws" {
 }
 
 resource "aws_security_group" "deploy" {
-  name   = "deploy-sg"
-  vpc_id = aws_vpc.deploy.id
+  name = "deploy-sg"
+  tags = {
+    Name = "deploy-sg"
+  }
 
   ingress {
     from_port   = 22
@@ -22,64 +24,13 @@ resource "aws_security_group" "deploy" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # 適宜変更
   }
-
-  tags = {
-    Name = "deploy-sg"
-  }
-}
-
-resource "aws_vpc" "deploy" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "deploy-vpc"
-  }
-}
-
-resource "aws_subnet" "deploy" {
-  vpc_id            = aws_vpc.deploy.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-northeast-1a"
-
-  tags = {
-    Name = "deploy-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "deploy" {
-  vpc_id = aws_vpc.deploy.id
-
-  tags = {
-    Name = "deploy-igw"
-  }
-}
-
-resource "aws_route_table" "deploy" {
-  vpc_id = aws_vpc.deploy.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.deploy.id
-  }
-
-  tags = {
-    Name = "deploy-rt"
-  }
-}
-
-resource "aws_route_table_association" "deploy" {
-  subnet_id      = aws_subnet.deploy.id
-  route_table_id = aws_route_table.deploy.id
 }
 
 resource "aws_instance" "deploy" {
   ami                         = "ami-03f584e50b2d32776"  # AL2023
   instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.deploy.id
-  vpc_security_group_ids      = [aws_security_group.deploy.id]
   key_name                    = "hiyama-diagram"
+  vpc_security_group_ids      = [aws_security_group.deploy.id]
   associate_public_ip_address = true
 
   tags = {
